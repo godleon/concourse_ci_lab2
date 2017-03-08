ad8879e0ae6aa3addf16bf5d8b0e7cb2b298f96c
godleon <godleon@gmail.com>
2017-03-08 08:42:24 +0800

    rally test

--- scripts/discovery.sh
+++ scripts/discovery.sh
@@ -143,8 +143,8 @@ fi
 #PATH=${PATH}:/opt/rh/python27/root/usr/bin
 
 # 設定 tempest
-mkdir /tempest
-#cd /tempest
+#mkdir -p /tempest
+cd /tempest
 #ln -s /usr/share/openstack-tempest-13.0.0 /usr/share/openstack-tempest
 #sh /usr/share/openstack-tempest/tools/configure-tempest-directory
 tools/config_tempest.py --debug --create identity.uri ${OS_AUTH_URL} identity.admin_username ${OS_USERNAME} identity.admin_password ${OS_PASSWORD} identity.admin_tenant_name ${OS_TENANT_NAME} object-storage.operator_role swiftoperator
